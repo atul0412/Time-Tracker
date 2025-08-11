32d8a20ec764e0b4d0ac695603ee4c7893245fc5
Atul Pandey <atulpandey0412@gmail.com>
2025-08-11 10:23:33 +0530

    update the report page

--- timesheet-client/src/app/reports/page.jsx
+++ timesheet-client/src/app/reports/page.jsx
@@ -77,6 +77,7 @@ export default function ReportPage() {
     }
     fetchAllProjects()
   }, [viewMode])
+
   // Fetch timesheets when relevant dependencies change
   useEffect(() => {
     const fetchTimesheets = async () => {
