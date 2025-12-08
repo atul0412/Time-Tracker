b4e30b60a8c061893ef7797f9bee1685bc273532
atul <atul@zool.in>
2025-12-08 16:32:47 +0530

    update the version of next

--- timesheet-server/routes/user.routes.js
+++ timesheet-server/routes/user.routes.js
@@ -16,7 +16,7 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/logout", protect, logoutUser);
+router.post("/logout",logoutUser);
 router.get("/getAlluser", protect, getAllUsers);
 router.delete("/:id", protect, deleteUser);
 router.put('/:id', protect, adminOnly, updateUser);
