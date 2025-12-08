39a0f74a874697090f632b939ade024bea64b5ab
atul <atul@zool.in>
2025-12-08 18:14:28 +0530

    upadate

--- timesheet-server/utils/sendEmail.js
+++ timesheet-server/utils/sendEmail.js
@@ -5,8 +5,9 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASS,
+    user: 'atul@zool.in',
+    pass: 'adlqbmcuznmqqzrh'
+,
   },
 });
 
