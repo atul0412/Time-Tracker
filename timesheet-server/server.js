b427ddab941f3cc25eea14fbcaacfd82dfa9c3c9
atul <atul@zool.in>
2025-12-10 12:23:00 +0530

    update the cors setting

--- timesheet-server/server.js
+++ timesheet-server/server.js
@@ -20,13 +20,20 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: [
-    process.env.FRONTEND_URL  // for local testing
-  ],
-  methods: "GET,POST,PUT,DELETE",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://time-sheet-039j-cztdt77pz-atul0412s-projects.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+// Must handle preflight
+app.options("*", cors());
 
 
 // Apply audit logging middleware before routes
